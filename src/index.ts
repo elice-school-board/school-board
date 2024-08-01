0b176dd56754079e41cde7d57d374b2d88ba72e9
heochangwon <wonn22@gmail.com>
2024-08-01 16:59:16 +0900

    chore: 데이터베이스 연결 및 서버 생성

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import AppDataSource from "./config/ormconfig";
 
 const app = express();
 
@@ -12,6 +13,12 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("안녕하세요, 기본 페이지입니다.");
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((error) => console.log(error));
 
 const PORT = 8000;
 app.listen(PORT, () => {
