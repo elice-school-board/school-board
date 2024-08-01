0463987161ee7b7cd6d5b8517270e6e1988a6b9a
heochangwon <wonn22@gmail.com>
2024-08-01 16:30:55 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// 기본 페이지
+app.get("/", (req, res) => {
+  res.send("안녕하세요, 기본 페이지입니다.");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
