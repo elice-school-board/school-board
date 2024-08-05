abd68db91b80879b8b4b91bb1b34075b68db09d6
heochangwon <wonn22@gmail.com>
2024-08-05 21:58:25 +0900

    feat: 라우터에 로그인 미들웨어 추가

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,20 +1,21 @@
 import { Router } from 'express';
 import { CommentController } from '../controllers/commentController';
+import { authMiddleware } from '../middlewares/auth';
 
 const router = Router();
 // 댓글 생성
-router.post('/posts/:postId/comments', CommentController.createComment);
+router.post('/posts/:postId/comments', authMiddleware, CommentController.createComment);
 
 // 특정 게시글의 댓글 및 대댓글 조회
-router.get('/posts/:postId/comments', CommentController.getCommentsByPostId);
+router.get('/posts/:postId/comments', authMiddleware, CommentController.getCommentsByPostId);
 
 // 특정 게시글의 베스트 댓글 조회
-router.get('/posts/:postId/comments/best-comment', CommentController.getBestCommentByPostId);
+router.get('/posts/:postId/comments/best-comment', authMiddleware, CommentController.getBestCommentByPostId);
 
 // 댓글 수정
-router.put('/comments/:id', CommentController.updateComment);
+router.put('/comments/:id', authMiddleware, CommentController.updateComment);
 
 // 댓글 삭제
-router.delete('/comments/:id', CommentController.deleteComment);
+router.delete('/comments/:id', authMiddleware, CommentController.deleteComment);
 
 export default router;
