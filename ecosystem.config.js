7c6c7c5add3cd868e52dbac6019bb808bf0378c9
heochangwon <wonn22@gmail.com>
2024-08-10 17:28:37 +0900

    chore: max_memory 4gb에서 1gb로 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
             instances: 1, // 여러 인스턴스를 실행할 경우, 기본값 1
             autorestart: true, // 애플리케이션이 중지되었을 때 자동으로 재시작
             watch: false, // 파일 변경 시 자동 재시작 여부 (true로 설정 시 개발 환경에 유용)
-            max_memory_restart: '4G', // 메모리 사용량이 4GB를 초과하면 재시작
+            max_memory_restart: '1G', // 메모리 사용량이 1GB를 초과하면 재시작
             env: {
                 NODE_ENV: process.env.NODE_ENV || 'development',
                 PORT: process.env.PORT,
